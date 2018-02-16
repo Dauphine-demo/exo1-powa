8ae2d541106af80ec5ee4672f9427a8ee3326641
jgay-dauphine <jgay.work@gmail.com>
2018-02-16 16:03:18 +0100

    Ajout du script

---
+++ webEuroDollar.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+import bs4
+import requests
+
+# Récupérer la page web
+url = 'http://www.boursorama.com/taux-de-change-euro-dollar-eur-usd'
+maRequete = requests.get(url)
+
+if( maRequete.status_code != 200 ):
+    print("Erreur lors de la récupération. Code d'erreur :", maRequete.status_code)
+    exit(1)
+
+print(maRequete.url)
+# Le contenu de la page est dans maRequete.text
+
+# Analyser la page web avec BS4
+soupe = bs4.BeautifulSoup(maRequete.text, "lxml")
+
+soupe2 = soupe.find('div', attrs={'id': 'fiche_cours_details'})
+conversion = soupe2.find('table').find('tr').findAll('td')[1].find('b').find('span').text
+
+# Afficher la valeur de la conversion Euro/Dollar
+print(conversion)
